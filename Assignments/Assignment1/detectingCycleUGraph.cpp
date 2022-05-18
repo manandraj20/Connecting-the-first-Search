3151317daf79d600295917628e5fbd2917cfc39d
Milan anand raj <84122339+manandraj20@users.noreply.github.com>
2022-05-19 01:38:28 +0530

    Files Updated

--- Assignments/Assignment1/detectingCycleUGraph.cpp
+++ Assignments/Assignment1/detectingCycleUGraph.cpp
@@ -1,18 +1,15 @@
 #include <iostream>
 #include <bits/stdc++.h>
+#include <cstdlib>
 using namespace std;
-int cycle_start = -1;// Track the start index of cycle if there is one.
+int cycle_start = -1;
 int cycle_end = -1;
 
-/*
-Creating a class Grapn and having a public member function isCyclic() to check for cycle in a graph.
-This solution considers that the vertices are numbered 1 through n, where n is the number of vertices.
-*/
 class Graph
 {
     int n;
     list<int> *adj;
-    bool dfs(int v, bool visited[], vector<int> &parent, int parent);
+    bool dfs(int v, bool visited[], vector<int> &parent, int par);
     bool bfs(int v, bool visited[], vector<int> &parent, vector<int> &distance);
 
 public:
@@ -36,22 +33,14 @@ bool Graph::dfs(int v, bool visited[], vector<int> &parent, int par)
     for (auto it = adj[v].begin(); it != adj[v].end(); ++it)
     {
         if ((*it) == par)
-            continue; // The edge joining current vertex to its parent is skipped.
-        if (visited[*it]) //If an already visited node (other than parent node) is encountered, there is a cycle.
+            continue;
+        if (visited[*it])
         {
             cycle_end = v;
             cycle_start = *it;
             return true;
         }
-        
-        /*
-         If there is no back edge. In DFS, (undirected graph) we can only tree edges and back edges, there is no chance for cross-edge.
-         However, one can come across cross-edge in BFS. Forward edge is never possible in undirected graph.
-         By cross-edge in BFS, we mean the edge which connects nodes in different branches.
-        */
-        
-        parent[*it] = v; 
-        
+        parent[*it] = v;
         if (dfs(*it, visited, parent, parent[*it]))
             return true;
     }
@@ -99,48 +88,40 @@ bool Graph::isCyclic()
     {
         visited[i] = false;
     }
-    /*
-    When Using one of BFS or DFS, comment out the other not is use.
-    Here DFS is in use. So, BFS has been commented out.
-    */
     // Using BFS
-//     for (int v = 1; v <= n; v++)
-//     {
-//         if (!visited[v] && bfs(v, visited, parent, distance))
-//             break;
-//     }
-    // Using DFS
     for (int v = 1; v <= n; v++)
     {
-        if (!visited[v] && dfs(v, visited, parent, parent[v])) // checking through all connected components
+        if (!visited[v] && bfs(v, visited, parent, distance))
             break;
     }
-    if (cycle_start == -1) // There was no cycle
+    // Using DFS
+    // for (int v = 1; v <= n; v++)
+    // {
+    //     if (!visited[v] && dfs(v, visited, parent, parent[v]))
+    //         break;
+    // }
+    if (cycle_start == -1)
     {
         return false;
     }
+    // return true;
     vector<int> cycle;
-    vector<int> cycle_2; // Used when there is a cross-edge in BFS.
+    vector<int> cycle_2;
     cycle.push_back(cycle_start);
-    for (int v = cycle_end; v != cycle_start; v = parent[v]) // loop through all the elements of cycle
+    // cout<<cycle_end<<" "<<" "<<parent[cycle_end]<<" " <<cycle_start<<"\n";
+    // cout<<parent[parent[cycle_end]]<<"\n";
+    for (int v = cycle_end; v != cycle_start; v = parent[v])
     {
-        /*
-        The following IF block is executed only if we do BFS and in BFS we encounter our cycle through cross-edge. 
-        By cross-edge in BFS, we mean the edge which connects nodes(i.e.,vertices) in different branches.
-        Also there is a theorem:
-        Let T be a BFS tree, let x and y be nodes in T belonging tp layers i and j and let (x, y) be and edge of G.
-        Then i and j differ by atmost 1.
-        
-        */
-        if ((distance[cycle_start] == distance[cycle_end]) || (distance[cycle_start] - distance[cycle_end] == 1))
+        //  cout<<distance[cycle_end]-distance[cycle_start]<<"\n";
+        if ((distance[cycle_start] == distance[cycle_end]) || (distance[cycle_end] - distance[cycle_start] == -1))
         {
             int w = cycle_start;
-            if (distance[cycle_start] - distance[cycle_end] == 1)
+            if (distance[cycle_end] - distance[cycle_start] == -1)
             {
-                
-                w = parent[w];     // The distance of node cycle_start is 1 more.
+                //  cycle.push_back(v);
+                w = parent[w];
 
-                while (w != v)     // check till the common parent vertex is reached.
+                while (w != v)
                 {
                     cycle.push_back(w);
                     cycle_2.push_back(v);
@@ -149,53 +130,55 @@ bool Graph::isCyclic()
                 }
                 cycle.push_back(w);
             }
-            else // when both cycle_start and cycle_end node is at the same level.
+            else
             {
+                //  cycle.push_back(v);
                 while (w != v)
                 {
+                    //   cycle_2.push_back(w);
                     cycle.push_back(v);
                     v = parent[v];
                     w = parent[w];
                     cycle_2.push_back(w);
                 }
+                //  cycle.push_back(w);
             }
             break;
         }
-        
+
         cycle.push_back(v);
     }
-    if (cycle_2.size() > 0) // when cross-edge in BFS is found.
+    if (cycle_2.size() > 0)
     {
         reverse(cycle_2.begin(), cycle_2.end());
         cycle.insert(cycle.end(), cycle_2.begin(), cycle_2.end());
     }
+
     cycle.push_back(cycle_start);
+
+    // reverse(cycle.begin(), cycle.end());
     cout << cycle.size() << "\n";
     for (int v : cycle)
         cout << v << " ";
     return true;
 }
 
-int main() //driver code
+int main()
 {
-    
+    // your code goes here
     int n, m;
-    cout << "Give n and m:"
-         << "\n";
-    cin >> n >> m; // Taking inputs
+
+    cin >> n >> m;
     Graph g(n);
     int a, b;
     for (int i = 0; i < m; i++)
     {
-        cout << "Give a and b"
-             << "\n";
-        cin >> a >> b; //Taking inputs
+        cin >> a >> b;
         g.addEdge(a, b);
         g.addEdge(b, a);
     }
     if (!g.isCyclic())
-        cout << "IMPOSSIBLE"
-             << "\n";
+        cout << "IMPOSSIBLE";
 
     return 0;
 }
